0cb92d03e8be4bec6a9c97e76d434da03524bf9b
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-05-14 19:41:02 -0300

    Adds @apollo/federation dependency to the classroom project

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -1,6 +1,9 @@
 import path from 'node:path';
 
-import { ApolloDriver } from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -17,8 +20,8 @@ import { StudentsResolver } from './graphql/resolvers/students.resolver';
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
