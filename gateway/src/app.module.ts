d0264963e04eed8db3e200640b219b4e70b56490
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-05-15 18:45:26 -0300

    Updates graphql dependencies

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,36 +1,39 @@
+/* eslint-disable dot-notation */
 import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
-    driver: ApolloGatewayDriver,
-    server: {
-      cors: true,
-      context: ({ req }) => {
-        return { headers: req.headers };
-      }
-    },
-    gateway: {
-      supergraphSdl: new IntrospectAndCompose({
-        subgraphs: [
-          { name: 'classroom', url: 'http://localhost:3002/graphql' },
-          { name: 'purchases', url: 'http://localhost:3003/graphql' },
-        ]
-      }),
-      buildService: ({ url }) => {
-        return new RemoteGraphQLDataSource({
-          url,
-          willSendRequest: ({ request, context }) => {
-            request.http.headers.set(
-              'authorization',
-              context?.['headers']?.['authorization']
-            );
-          }
-        })
-      }
-    }
-  })],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+        context: ({ req }) => {
+          return { headers: req.headers };
+        },
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'purchases', url: 'http://localhost:3003/graphql' },
+            { name: 'classroom', url: 'http://localhost:3002/graphql' },
+          ],
+        }),
+        buildService: ({ url }) => {
+          return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }) {
+              request.http.headers.set(
+                'authorization',
+                context?.['headers']?.['authorization'],
+              );
+            },
+          });
+        },
+      },
+    }),
+  ],
 })
 export class AppModule { }
